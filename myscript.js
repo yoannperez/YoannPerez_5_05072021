e08cc369a87a62306720bf8500319dc7a83b91cd
Yoann Perez <yoann.perez@gmail.com>
2021-07-12 18:41:02 +0200

    Recieve information from backend

---
+++ myscript.js
@@ -0,0 +1,58 @@
+fetch("http://localhost:3000/api/cameras")
+  .then(function (res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function (value) {
+    let datas = value;
+    showCameras(datas);
+    
+  })
+  .catch(function (err) {
+    window.alert("Erreur de connection réseau");
+  });
+
+const header = document.querySelector("header");
+const section = document.querySelector("section");
+
+
+
+function showCameras(myCameras) {
+  let thisCamera = myCameras;
+
+  for (var i = 0; i < thisCamera.length; i++) {
+    let myCamera = document.createElement("article");
+    let myName = document.createElement("h2");
+    let myId = document.createElement("p");
+    let myPrice = document.createElement("p");
+    let myDescription = document.createElement("p");
+    let myLenses = document.createElement("ul");
+    let myImage = document.createElement("div");
+
+    myName.textContent = "Model: " + thisCamera[i].name;
+    myId.textContent = "ID: " + thisCamera[i]._id;
+    myPrice.textContent = "Prix: " + thisCamera[i].price;
+    myDescription.textContent = "Model: " + thisCamera[i].description;
+    myImage.innerHTML = `<img src="${thisCamera[i].imageUrl}" alt="${thisCamera[i].name}">`;
+
+    // List Lenses
+    var lenses = thisCamera[i].lenses;
+    for (var j = 0; j < lenses.length; j++) {
+      var listItem = document.createElement("li");
+      listItem.textContent = lenses[j];
+      myLenses.appendChild(listItem);
+    }
+
+
+    //Create DOM
+    myCamera.appendChild(myName);
+    myCamera.appendChild(myId);
+    myCamera.appendChild(myPrice);
+    myCamera.appendChild(myDescription);
+    myCamera.appendChild(myLenses);
+    myCamera.appendChild(myImage);
+
+    section.appendChild(myCamera).classList.add("red");
+  }
+}
