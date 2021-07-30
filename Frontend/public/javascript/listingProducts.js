b34b059f8337f9d7e67d826959f61224d30d8300
Yoann Perez <yoann.perez@gmail.com>
2021-07-30 08:15:38 +0200

    Work in progress

--- Frontend/public/javascript/listingProducts.js
+++ Frontend/public/javascript/listingProducts.js
@@ -5,18 +5,16 @@ fetch("http://localhost:3000/api/cameras").then(data => data.json())
         .then(jsonListArticle => {
             for (let jsonArticle of jsonListArticle) {
                 let product = new Article(jsonArticle);
-
-
                 let price = formatPrice(product.price);
-
+                
                 // List Lenses
-                // var lenses = product.lenses;
-                // for (let j in lenses) {
-                // var listItem = document.createElement("li");
-
-                // listItem.textContent = lenses[j];
-                // myLenses.appendChild(listItem);
-                // }
+                let lenses = product.lenses;
+                let myLenses = document.createElement("div");
+                for (let j in lenses) {
+                    var listItem = document.createElement("li");
+                    listItem.textContent = lenses[j];
+                    myLenses.appendChild(listItem);
+                    }
                 
                 document.querySelector(".container").innerHTML += `<article class="container bg-light border rounded-2 shadow p-4">
                                                                         <div class="row p-2">
@@ -27,9 +25,10 @@ fetch("http://localhost:3000/api/cameras").then(data => data.json())
                                                                                 <p >${product.description}</p>
                                                                             </div>
                                                                             <img class="col-lg-3 img-fluid img-thumbnail rounded mx-auto d-block shadow-sm" src="${product.imageUrl}" alt="${product.name}">
-                                                                            
+                                                                            ${myLenses}
                                                                         </div>
                                                                     `;
+                                                                 
             }
 
             
