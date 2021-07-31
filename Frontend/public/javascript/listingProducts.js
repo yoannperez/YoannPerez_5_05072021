a52d6eb0b9679b98002290447bad70614902f52a
Yoann Perez <yoann.perez@gmail.com>
2021-07-31 11:05:34 +0200

    Add js IndexPage

--- Frontend/public/javascript/listingProducts.js
+++ Frontend/public/javascript/listingProducts.js
@@ -6,16 +6,20 @@ fetch("http://localhost:3000/api/cameras").then(data => data.json())
             for (let jsonArticle of jsonListArticle) {
                 let product = new Article(jsonArticle);
                 let price = formatPrice(product.price);
+                let id = jsonArticle._id;
+                
                 
                 // List Lenses
                 let lenses = product.lenses;
-                let myLenses = document.createElement("div");
+                let myLenses = document.createElement("ul");
                 for (let j in lenses) {
                     var listItem = document.createElement("li");
                     listItem.textContent = lenses[j];
+                    // listItem.classList.add("list-inline-item")
                     myLenses.appendChild(listItem);
                     }
-                
+
+                // myLenses.classList.add("list-inline")
                 document.querySelector(".container").innerHTML += `<article class="container bg-light border rounded-2 shadow p-4">
                                                                         <div class="row p-2">
                                                                             <div class="col-lg-9">
@@ -23,13 +27,20 @@ fetch("http://localhost:3000/api/cameras").then(data => data.json())
                                                                                 <hr/>
                                                                                 <p class="text-muted">Depuis ${price}€</p>
                                                                                 <p >${product.description}</p>
+                                                                                <div class="row">
+                                                                                    <div class="col-3">
+                                                                                    <p>Optiques : <p>
+                                                                                    </div>
+                                                                                    <div class="col-8 ">
+                                                                                    ${myLenses.outerHTML}
+                                                                                    </div>
+                                                                                </div>
+                                                                                
                                                                             </div>
                                                                             <img class="col-lg-3 img-fluid img-thumbnail rounded mx-auto d-block shadow-sm" src="${product.imageUrl}" alt="${product.name}">
-                                                                            ${myLenses}
+                                                                            
                                                                         </div>
                                                                     `;
-                                                                 
+                
             }
-
-            
         });
