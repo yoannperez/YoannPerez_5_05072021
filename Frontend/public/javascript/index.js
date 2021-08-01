51cb0bd0aa69872d2924cf61bed11cda7545d425
Yoann Perez <yoann.perez@gmail.com>
2021-08-01 18:55:22 +0200

    Add link to product page

--- Frontend/public/javascript/index.js
+++ Frontend/public/javascript/index.js
@@ -20,7 +20,8 @@ fetch("http://localhost:3000/api/cameras").then(data => data.json())
                     }
 
                 // myLenses.classList.add("list-inline")
-                document.querySelector(".container").innerHTML += `<article class="container bg-light border rounded-2 shadow p-4">
+                document.querySelector(".container").innerHTML += `
+                                                                        <article class="container bg-light border rounded-2 shadow p-4">
                                                                         <div class="row p-2">
                                                                             <div class="col-lg-9">
                                                                                 <h2 class="h2 ">${product.name}</h2>
@@ -31,16 +32,20 @@ fetch("http://localhost:3000/api/cameras").then(data => data.json())
                                                                                     <div class="col-3">
                                                                                     <p>Optiques : <p>
                                                                                     </div>
-                                                                                    <div class="col-8 ">
+                                                                                    <div class="col-6 ">
                                                                                     ${myLenses.outerHTML}
                                                                                     </div>
+                                                                                    <a href='article.html?id=${id}' class="col-3 ">
+                                                                                    <button type="button" class="btn btn-warning">Voir le produit</button>
+                                                                                    </a>
                                                                                 </div>
                                                                                 
                                                                             </div>
                                                                             <img class="col-lg-3 img-fluid img-thumbnail rounded mx-auto d-block shadow-sm" src="${product.imageUrl}" alt="${product.name}">
                                                                             
                                                                         </div>
+                                                                        
                                                                     `;
-                
+                                                                   
             }
         });
---
+++ Frontend/public/javascript/productPage.js
@@ -0,0 +1,23 @@
+let id = "5be1ed3f1c9d44000030b061";
+
+
+// Gestion page d'acceuil
+//Appeller un article en particulier
+function getArticleById(id){
+    let objToCall = "http://localhost:3000/api/cameras/"+id;
+    console.log(objToCall);
+  fetch(objToCall)
+    .then(function (res) {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then(function (value) {
+      let monObjet = value;
+      console.log("Retour appel objet: " + id);
+      console.log(monObjet);  
+    })
+    .catch(function (err) {
+      window.alert("Erreur de connection réseau");
+    });
+  }
