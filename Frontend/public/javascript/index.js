91cc95c6dac3518e82b3776df518f67ae3623a7b
Yoann Perez <yoann.perez@gmail.com>
2021-08-02 18:10:08 +0200

    Before changin t select form in product page

--- Frontend/public/javascript/index.js
+++ Frontend/public/javascript/index.js
@@ -22,29 +22,27 @@ fetch("http://localhost:3000/api/cameras").then(data => data.json())
                 // myLenses.classList.add("list-inline")
                 document.querySelector(".container").innerHTML += `
                                                                         <article class="container bg-light border rounded-2 shadow p-4">
-                                                                        <div class="row p-2">
-                                                                            <div class="col-lg-9">
-                                                                                <h2 class="h2 ">${product.name}</h2>
-                                                                                <hr/>
-                                                                                <p class="text-muted">Depuis ${price}€</p>
-                                                                                <p >${product.description}</p>
-                                                                                <div class="row">
-                                                                                    <div class="col-3">
-                                                                                    <p>Optiques : <p>
+                                                                            <div class="row p-2">
+                                                                                <div class="col-lg-9">
+                                                                                    <h2 class="h2 ">${product.name}</h2>
+                                                                                    <hr/>
+                                                                                    <p class="text-muted">Depuis ${price}€</p>
+                                                                                    <p >${product.description}</p>
+                                                                                    <div class="row">
+                                                                                        <div class="col-2">
+                                                                                        <p>Optiques : <p>
+                                                                                        </div>
+                                                                                        <div class="col-6 ">
+                                                                                        ${myLenses.outerHTML}
+                                                                                        </div>
                                                                                     </div>
-                                                                                    <div class="col-6 ">
-                                                                                    ${myLenses.outerHTML}
-                                                                                    </div>
-                                                                                    <a href='article.html?id=${id}' class="col-3 ">
-                                                                                    <button type="button" class="btn btn-warning">Voir le produit</button>
-                                                                                    </a>
                                                                                 </div>
-                                                                                
+                                                                                <img class="col-lg-3 img-fluid img-thumbnail rounded mx-auto d-block shadow-sm" src="${product.imageUrl}" alt="${product.name}">
                                                                             </div>
-                                                                            <img class="col-lg-3 img-fluid img-thumbnail rounded mx-auto d-block shadow-sm" src="${product.imageUrl}" alt="${product.name}">
-                                                                            
-                                                                        </div>
-                                                                        
+                                                                            <a href='article.html?id=${id}' class="row" style="text-decoration: none">
+                                                                                <button type="button" class="btn btn-warning col-4 offset-4" >Voir le produit</button>
+                                                                            </a>
+                                                                        </article>
                                                                     `;
                                                                    
             }
