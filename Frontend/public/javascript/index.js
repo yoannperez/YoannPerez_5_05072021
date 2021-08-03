6139c512a27926c45bf986c00e3c6f29f63b814f
Yoann Perez <yoann.perez@gmail.com>
2021-08-03 07:07:44 +0200

    Add option Selected listener

--- Frontend/public/javascript/index.js
+++ Frontend/public/javascript/index.js
@@ -21,24 +21,26 @@ fetch("http://localhost:3000/api/cameras").then(data => data.json())
 
                 // myLenses.classList.add("list-inline")
                 document.querySelector(".container").innerHTML += `
-                                                                        <article class="container bg-light border rounded-2 shadow p-4">
+                                                                        <article class="container bg-light border rounded-2 shadow mb-10 p-4">
                                                                             <div class="row p-2">
-                                                                                <div class="col-lg-9">
+                                                                                <div class="col-lg-8">
                                                                                     <h2 class="h2 ">${product.name}</h2>
                                                                                     <hr/>
                                                                                     <p class="text-muted">Depuis ${price}€</p>
                                                                                     <p >${product.description}</p>
+                                                                                    <hr/>
                                                                                     <div class="row">
-                                                                                        <div class="col-2">
-                                                                                        <p>Optiques : <p>
+                                                                                        <div class="col-3">
+                                                                                        <p>Options disponibles: <p>
                                                                                         </div>
                                                                                         <div class="col-6 ">
                                                                                         ${myLenses.outerHTML}
                                                                                         </div>
                                                                                     </div>
                                                                                 </div>
-                                                                                <img class="col-lg-3 img-fluid img-thumbnail rounded mx-auto d-block shadow-sm" src="${product.imageUrl}" alt="${product.name}">
+                                                                                <img class="col-lg-4 img-fluid img-thumbnail rounded mx-auto d-block shadow-sm myimage" src="${product.imageUrl}" alt="${product.name}">
                                                                             </div>
+                                                                            <hr/>
                                                                             <a href='article.html?id=${id}' class="row" style="text-decoration: none">
                                                                                 <button type="button" class="btn btn-warning col-4 offset-4" >Voir le produit</button>
                                                                             </a>
--- Frontend/public/javascript/productPage.js
+++ Frontend/public/javascript/productPage.js
@@ -30,6 +30,7 @@ function buildDom(articleToshow) {
   let myLenses = document.createElement("select");
   myLenses.classList.add("form-select")
   myLenses.setAttribute("aria-label", "Sélectionner votre option");
+  myLenses.setAttribute("id", "optionChoice");
   let firstItem = document.createElement("option");
   firstItem.innerHTML = "Choisisez votre optique";
   myLenses.appendChild(firstItem);
@@ -38,7 +39,7 @@ function buildDom(articleToshow) {
     var listItem = document.createElement("option");
     
     
-    listItem.setAttribute("value", `value=${j}`);
+    listItem.setAttribute("value", `${lenses[j]}`);
     listItem.textContent = lenses[j];
     myLenses.appendChild(listItem);
     }
@@ -46,7 +47,7 @@ function buildDom(articleToshow) {
   document.querySelector("#productContainer").innerHTML += `
       <article class="container bg-light border rounded-2 shadow p-4">
       <div class="row p-2">
-      <img class="col-lg-4 img-fluid img-thumbnail rounded mx-auto d-block shadow-sm" src="${product.imageUrl}" alt="${product.name}">
+      <img class="col-lg-4 img-fluid img-thumbnail rounded mx-auto d-block shadow-sm myimage" src="${product.imageUrl}" alt="${product.name}">
       </div>
       <div class="row p-2">
             
@@ -55,19 +56,20 @@ function buildDom(articleToshow) {
                 <hr/>
                 <p class="text-muted">Depuis ${price}€</p>
                 <p >${product.description}</p>
+                <hr/ class="mt-3"> 
                 <div class="row">
                   <div class="col-3">
-                    <p>Optiques : <p>
+                    <p>Personalisez votre appareil : <p>
                   </div>
                   <div class="col-6 ">
                     ${myLenses.outerHTML}
                   </div>
-                  <hr/>  
-            </div>
-              <div class="row p-2">
-                <button id="return" type="button" class="btn btn-warning col-4 offset-1" >Liste des produits</button>
-                <button id="addToCart" type="button" class="btn btn-danger col-4 offset-2" >Ajouter au panier</button>
-              </div>
+                  </div>
+                <hr/ class="mt-3">  
+                <div class="row p-2">
+                    <button id="return" type="button" class="btn btn-warning col-4 offset-1" >Liste des produits</button>
+                    <button id="addToCart" type="button" class="btn btn-danger col-4 offset-2" >Ajouter au panier</button>
+                </div>
             </div>
           
           </div>
@@ -82,8 +84,16 @@ returnBtn.addEventListener('click', function () {
 
     // Gestion de l'évènement click sur le bouton liste des produits.  
 const addToCartBtn = document.getElementById("addToCart");
-addToCartBtn.addEventListener('click', function () {
+addToCartBtn.addEventListener('select', function () {
     window.alert("Le panier c'est chô ! ")
     } )
+
+//Listen witch option is choised
+const optionChoised = document.getElementById("optionChoice");
+optionChoised.addEventListener('change', () => {
+console.log(optionChoised.value)
+}
+    )
+
 
 }
