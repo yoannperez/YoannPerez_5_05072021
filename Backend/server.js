e931afb0b34846bc0e661f1c9804f40a7f6cd69d
Yoann Perez <yoann.perez@gmail.com>
2021-12-30 11:47:18 +0100

    Add https support

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,11 @@
 const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const app = require('./app');
 
+// --------------- VERSION   HTTP ------------
+// --------------- VERSION   HTTP ------------
+// Normalisation port
 const normalizePort = val => {
   const port = parseInt(val, 10);
 
@@ -12,9 +17,12 @@ const normalizePort = val => {
   }
   return false;
 };
+
+//Déclaration du port
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
+// Errors Management
 const errorHandler = error => {
   if (error.syscall !== 'listen') {
     throw error;
@@ -33,13 +41,72 @@ const errorHandler = error => {
   }
 };
 
+// Create serverHttp 
 const server = http.createServer(app);
 
 server.on('error', errorHandler);
 server.on('listening', () => {
   const address = server.address();
   const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-  console.log('Listening on ' + bind);
+  console.log('HTTP Listening on ' + bind);
 });
 
 server.listen(port);
+
+////////// Version HTTPS /////////////////
+
+
+// Https SLL KEY AND CERTS //
+let sslOptions = {
+  key: fs.readFileSync('./ssl/example.com+4-key.pem'),
+  cert: fs.readFileSync('./ssl/example.com+4.pem')
+};
+
+// Normalisation port
+const normalizePortHttps = val => {
+  const portHttps = parseInt(val, 10);
+
+  if (isNaN(portHttps)) {
+    return val;
+  }
+  if (portHttps >= 0) {
+    return portHttps;
+  }
+  return false;
+};
+
+//Déclaration du port
+const portHttps = normalizePortHttps(process.env.PORT || '3001');
+app.set('port', portHttps);
+
+
+// Errors Management
+const errorHandlerHttps = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const addressHttps = serverHttps.address();
+  const bindHttps = typeof addressHttps  === 'string' ? 'pipe ' + addressHttps : 'port: ' + portHttps;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bindHttps + ' requires elevated privileges.');
+      process.exit(1);
+    case 'EADDRINUSE':
+      console.error(bindHttps + ' is already in use.');
+      process.exit(1);
+    default:
+      throw error;
+  }
+};
+
+// Create serverHttps 
+const serverHttps = https.createServer(sslOptions, app);
+
+serverHttps.on('error', errorHandlerHttps);
+serverHttps.on('listening', () => {
+  const addressHttps = serverHttps.address();
+  const bindHttps = typeof address === 'string' ? 'pipe ' + addressHttps : 'port ' + portHttps;
+  console.log('HTTPS Listening on ' + bindHttps);
+});
+
+serverHttps.listen(portHttps);
